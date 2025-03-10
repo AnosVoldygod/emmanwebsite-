d3ab5ee73312ee6ada37c6c2e90afabf80589a51
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-03-10 13:29:48 +1100

    added class to nav

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,11 @@ import ProfSummary from './components/ProfSummary.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <WelcomeSection username="Emmanuel Berkowicz" />
       <ProfSummary />
 
-      <nav>
+      <nav class="nav">
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
       </nav>
