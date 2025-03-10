5794fb0e623037e44efdca843ee870004edab598
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-03-10 21:17:22 +1100

    aboutvue: text added for about infovue: template points added

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="info">
-    <h1>
-      <p>This is the personal information page</p>
-    </h1>
+    <h1>Information</h1>
+    <h2>Qualifications:</h2>
+    <p>Master's in Computer Science</p>
+    <p>Bachelor's in Mechanical Engineering</p>
+    <h2>Contact:</h2>
+    <p>Mobile:</p>
+    <p>Email:</p>
+    <p>Linkedin:</p>
   </div>
 </template>
