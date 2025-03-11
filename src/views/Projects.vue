e06e3169beef6a09dd48d2c76ff9170c98d02264
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-03-11 21:41:43 +1100

    feat(Projects.vue): Shakespeare insult generator. Card style from BS assets: hipspeare image added.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,6 +4,22 @@
       <h1 style="text-align: center">LIST OF PROJECTS GO HERE</h1>
     </div>
   </header>
+
+  <main>
+    <div class="card" style="width: 18rem">
+      <img alt="Vue logo" class="logo" src="../assets/hipspeare.jpg" width="125" height="125" />
+      <div class="card-body">
+        <h5 class="card-title" style="text-align: center; color: black; font-family: cursive">
+          Shakespeare Insult Generator
+        </h5>
+        <p class="card-text">
+          This is a silly application which does what you'd expect, generates an insult in a
+          Shakespearean vernacular. You know, because regular insults are just to plebeian now.
+        </p>
+        <a href="#" class="btn btn-primary">Generate Thy Insult</a>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
