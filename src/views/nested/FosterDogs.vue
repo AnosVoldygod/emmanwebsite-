507f21b5b9e00c7a1b36e4106ef1302cd7cd862f
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-03-15 23:59:29 +1100

    AllFosterDogs moved into new folder FosterDogs FosterDog code updated to include table for all foster dogs

--- src/views/nested/FosterDogs.vue
+++ src/views/nested/FosterDogs.vue
@@ -18,10 +18,41 @@
       style="padding: 20px"
       alt="Dogs I've Fostered"
       class="fosterImage"
-      src="../../assets/AllFosterDogs.jpg"
+      src="../../assets/FosterDogs/AllFosterDogs.jpg"
       width="800"
       height="800"
     />
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Quirk</th>
+            <th scope="col">Image</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr>
+            <th scope="row">1</th>
+            <td>Blondie</td>
+            <td>Squeeky toy lover</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
 
