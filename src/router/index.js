1387492d4c876891bf17e662ad9813c9fbb3f923
Emmanuel Berkowicz <berkowiczemmanuel@gmail.com>
2025-03-22 12:38:00 +1100

    Removed info.vue and TheWelcome and WelcomeSEction

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
-import Info from '../views/Info.vue'
 import ForFun from '../views/ForFun.vue'
 import Login from '../views/Login.vue'
 import FosterDogs from '../views/nested/FosterDogs.vue'
@@ -10,7 +9,6 @@ import { isAuthenticated } from './auth.js'
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
-  { path: '/info', name: 'Info', component: Info },
   { path: '/ForFun/fosterdogs', name: 'FosterDogs', component: FosterDogs },
   { path: '/ForFun', name: 'ForFun', component: ForFun },
   { path: '/Login', name: 'Login', component: Login },
